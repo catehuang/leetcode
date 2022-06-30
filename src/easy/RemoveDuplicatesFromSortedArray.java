85c059179ae3ddb7e3e679363dca1e3e7c8e8b1a
Cate Huang <s0350228@gmail.com>
2022-06-30 17:37:10 -0600

    add Remove Duplicates from Sorted Array

---
+++ src/easy/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,19 @@
+package easy;
+
+public class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums)
+    {
+        int n = 0;
+
+        for (int i = 1; i < nums.length; i++)
+        {
+            if (nums[i] != nums[n])
+            {
+                n++;
+                nums[n] = nums[i];
+            }
+        }
+
+        return n + 1;
+   }
+}
