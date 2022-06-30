3b9fe9e991a82ac377969a22367ba16a2578d9ed
Cate Huang <s0350228@gmail.com>
2022-06-30 11:50:20 -0600

    added two sum

---
+++ src/easy/TwoSum.java
@@ -0,0 +1,22 @@
+package easy;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.length - 1; i++)
+        {
+            for (int j = i + 1; j < nums.length; j++)
+            {
+                if (nums[j] <= target) {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        int[] arr = {i, j};
+                        return arr;
+                    }
+                }
+
+            }
+        }
+        return null;
+    }
+}
---
+++ test/problemdomain/easy/TwoSumTest.java
@@ -0,0 +1,46 @@
+package easy;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TwoSumTest
+{
+    private TwoSum twoSum;
+
+    @BeforeEach
+    void setUp()
+    {
+        this.twoSum = new TwoSum();
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+        this.twoSum = null;
+    }
+
+    @Test
+    void testcase1()
+    {
+        int[] givenArr = {7, 5, 9, 5};
+        int target = 10;
+        int[] expected = {1, 3};
+        int[] output = this.twoSum.twoSum(givenArr, target);
+        assertEquals(Arrays.toString(expected), Arrays.toString(output));
+    }
+
+    @Test
+    void testcase2()
+    {
+        int[] givenArr = {0, 5, 9, 0};
+        int target = 0;
+        int[] expected = {0, 3};
+        int[] output = this.twoSum.twoSum(givenArr, target);
+        assertEquals(Arrays.toString(expected), Arrays.toString(output));
+    }
+}
