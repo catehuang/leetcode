16f64e33ee76553ab263830690c58807bf9969df
Cate Huang <s0350228@gmail.com>
2022-07-11 15:35:18 -0600

    add comment

--- src/easy/SameTree.java
+++ src/easy/SameTree.java
@@ -1,8 +1,11 @@
 package easy;
 
-import java.util.ArrayList;
-import java.util.List;
 
+/**
+ * Compares two trees
+ * Return true if two trees are the same
+ * Return false if two trees are not the same
+ */
 public class SameTree {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) {
