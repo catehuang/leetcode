bed8ca345792e3bdd7b42e8e1f2acb26cb1c9517
Cate Huang <s0350228@gmail.com>
2022-07-11 18:43:12 -0600

    add Max Depth of Bianry Tree

---
+++ test/problemdomain/easy/PathSumTest.java
@@ -0,0 +1,26 @@
+package easy;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PathSumTest {
+
+    PathSum pathSum;
+
+    @BeforeEach
+    void setUp() {
+        this.pathSum = new PathSum();
+    }
+
+    @AfterEach
+    void tearDown() {
+        this.pathSum = null;
+    }
+
+    @Test
+    void testcase1() {
+    }
+}
