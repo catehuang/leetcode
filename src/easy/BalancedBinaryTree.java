c0d5fd4b9afa2e8aaa2d0a7936942b2f8a6f0d1b
Cate Huang <s0350228@gmail.com>
2022-07-13 15:03:26 -0600

    add Balanced Binary Tree

---
+++ src/easy/BalancedBinaryTree.java
@@ -0,0 +1,16 @@
+package easy;
+
+/** Given a binary tree, determine if it is height-balanced.
+ *  For this problem, a height-balanced binary tree is defined as:
+ *  a binary tree in which the left and right subtrees of every node differ
+ *  in height by no more than 1.
+ *
+ *  Constraints:
+ *      The number of nodes in the tree is in the range [0, 5000].
+ *      -104 <= Node.val <= 104
+ */
+public class BalancedBinaryTree {
+    public boolean isBalanced(TreeNode root) {
+        
+    }
+}
