e7ceabe6ac9100cd35398bb133f61ad86dfb3851
Cate Huang <s0350228@gmail.com>
2022-07-15 18:00:04 -0600

    add Path Sum - slow and use lots of memory

--- test/problemdomain/easy/PathSumTest.java
+++ test/problemdomain/easy/PathSumTest.java
@@ -22,5 +22,33 @@ class PathSumTest {
 
     @Test
     void testcase1() {
+        TreeNode n4 = new TreeNode(4, new TreeNode(11, new TreeNode(7), new TreeNode(2)), null);
+        TreeNode n8 = new TreeNode(4, new TreeNode(13), new TreeNode(4, null, new TreeNode(1)));
+        TreeNode root = new TreeNode(5, n4, n8);
+
+        assertTrue(this.pathSum.hasPathSum(root, 22));
+    }
+
+    @Test
+    void testcase2() {
+        TreeNode root = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+
+        assertFalse(this.pathSum.hasPathSum(root, 5));
+
+    }
+
+    @Test
+    void testcase3() {
+        TreeNode root = new TreeNode(null);
+
+        assertFalse(this.pathSum.hasPathSum(root, 0));
+    }
+
+    @Test
+    void testcase4() {
+        TreeNode root = new TreeNode(-5);
+
+        assertFalse(this.pathSum.hasPathSum(root, 0));
+        assertTrue(this.pathSum.hasPathSum(root, -5));
     }
 }
