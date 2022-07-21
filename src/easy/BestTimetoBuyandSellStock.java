9f9c977a909012711b3e54b4287ca2f00ec90cad
Cate Huang <s0350228@gmail.com>
2022-07-20 19:24:27 -0600

    faster than 93.25%, memory usage less than 94.70% of Java submissions

--- src/easy/BestTimetoBuyandSellStock.java
+++ src/easy/BestTimetoBuyandSellStock.java
@@ -11,11 +11,27 @@ package easy;
 public class BestTimetoBuyandSellStock {
     public int maxProfit(int[] prices) {
         int profit = 0;
+        int minIndex = 0;
+        int maxIndex = 0;
 
-        for (int i = 0; i < prices.length; i++)
-            for (int j = i + 1; j < prices.length; j++)
-                if (prices[j] > prices[i])
-                    profit = (prices[j] - prices[i] > profit) ? prices[j] - prices[i] : profit;
+        for (int i = 1; i < prices.length; i++)
+        {
+            if (prices[i] < prices[minIndex]) {
+                minIndex = i;
+                maxIndex = i;
+            } else if (prices[i] > prices[maxIndex]) {
+                maxIndex = i;
+                profit = (prices[i] - prices[minIndex] > profit) ? prices[i] - prices[minIndex] : profit;
+            }
+            //System.out.println(i + " " + minIndex + " " + maxIndex + " " + profit);
+        }
         return profit;
+
+//        Too slow, rejected
+//        for (int i = 0; i < prices.length; i++)
+//            for (int j = i + 1; j < prices.length; j++)
+//                if (prices[j] > prices[i])
+//                    profit = (prices[j] - prices[i] > profit) ? prices[j] - prices[i] : profit;
+//        return profit;
     }
 }
--- test/problemdomain/easy/BestTimetoBuyandSellStockTest.java
+++ test/problemdomain/easy/BestTimetoBuyandSellStockTest.java
@@ -35,4 +35,20 @@ class BestTimetoBuyandSellStockTest {
 
         assertEquals(expected, this.bestTimetoBuyandSellStock.maxProfit(prices));
     }
+
+    @Test
+    void maxProfitTest3() {
+        int[] prices = new int[] {1, 2};
+        int expected = 1;
+
+        assertEquals(expected, this.bestTimetoBuyandSellStock.maxProfit(prices));
+    }
+
+    @Test
+    void maxProfitTest4() {
+        int[] prices = new int[] {7,1,5,3,6,4, 4,56,88,66,4,1,2,5,0,5,6,65,8,52,75,3,44,5,1,5,845,514,8,1,21,0};
+        int expected = 845;
+
+        assertEquals(expected, this.bestTimetoBuyandSellStock.maxProfit(prices));
+    }
 }
