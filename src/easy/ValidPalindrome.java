7c9298598f1395058c0673a7927938a523aac090
catehuang <s0350228@gmail.com>
2022-07-25 17:45:22 -0600

    new create ValidPalindrome

---
+++ src/easy/ValidPalindrome.java
@@ -0,0 +1,19 @@
+package easy;
+
+/** A phrase is a palindrome if, after converting all uppercase letters into lowercase letters
+ *  and removing all non-alphanumeric characters, it reads the same forward and backward.
+ *  Alphanumeric characters include letters and numbers.
+ *  Constraints:
+ *      1 <= s.length <= 2 * 105
+ *      s consists only of printable ASCII characters.
+ */
+public class ValidPalindrome {
+    /**
+     * Given a string s, return true if it is a palindrome, or false otherwise.
+     * @param s a given string
+     * @return false if it is not a palindrome
+     */
+    public boolean isPalindrome(String s) {
+
+    }
+}
