dced74c5b6d474b0b62899c03d878f37a15537a7
catehuang <s0350228@gmail.com>
2022-11-04 11:47:15 -0600

    add 344 reverse string

---
+++ test/problemdomain/easy/_344_ReverseStringTest.java
@@ -0,0 +1,36 @@
+package easy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class _344_ReverseStringTest {
+
+    private _344_ReverseString reverseString;
+
+    @BeforeEach
+    public void setUp() {
+        reverseString = new _344_ReverseString();
+    }
+
+    @Test
+    public void reverseString_case_1() throws Exception {
+        char[] c = new char[] {'h','e','l','l','o'};
+        char[] output = new char[] {'o','l','l','e','h'};
+        char[] result = reverseString.reverseString(c);
+        for (int i = 0; i < c.length - 1 ; i++) {
+            assertEquals(output[i], result[i]);
+        }
+    }
+
+    @Test
+    public void reverseString_case_2() throws Exception {
+        char[] c = new char[] {'H','a','n','n','a','h'};
+        char[] output = new char[] {'h','a','n','n','a','H'};
+        char[] result = reverseString.reverseString(c);
+        for (int i = 0; i < c.length - 1 ; i++) {
+            assertEquals(output[i], result[i]);
+        }
+    }
+}
