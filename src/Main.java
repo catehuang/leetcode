ba2c1f3c04e199f41f916b358392dcbf9eb894cc
catehuang <s0350228@gmail.com>
2022-11-04 11:47:28 -0600

    add comments

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
-import easy.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
 
-import java.util.Arrays;
-
-public class Main
-{
+class Main {
     public static void main(String[] args)
     {
+        List<String> list1 = new ArrayList<>();
+        list1.add( "One" );
+        list1.add( "Two" );
+        list1.add( "Three" );
 
+        List<String> list2 = new ArrayList<>();
+        list2.add("Four");
+
+        list1.remove( list2 );
+        System.out.println(list1);
     }
 }
+
