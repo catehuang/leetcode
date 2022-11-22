77b57d502276b1786978cc8e8abfedaf4401425f
catehuang <s0350228@gmail.com>
2022-11-21 19:03:19 -0700

    add method into tree

--- src/common/TreeNode.java
+++ src/common/TreeNode.java
@@ -1,5 +1,7 @@
 package common;
 
+import com.sun.source.tree.Tree;
+
 public class TreeNode {
     public Integer val;
     public TreeNode left;
@@ -20,4 +22,31 @@ public class TreeNode {
         this.right = right;
     }
 
+    public TreeNode addNode (TreeNode root, int value) {
+        //is the root exists?
+        if (root == null) {
+            // create a new node
+            root = new TreeNode(value);
+            System.out.println("Add a node " + value + " to the root.");
+        } else {
+            // is the left leaf empty?
+            if (root.left == null) {
+                root.left = new TreeNode(value);
+                System.out.println("Add a node " + value + " to the left of node " + root.val);
+                // is the right leaf empty?
+            } else if (root.right == null) {
+                root.right = new TreeNode(value);
+                System.out.println("Add a node " + value + " to the right of node " + root.val);
+                // there's no room of this root
+                // is there sibling of this root
+            } else {
+                if (root.left.left == null || root.left.right == null) {
+                    addNode(root.left, value);
+                } else {
+                    addNode(root.right, value);
+                }
+            }
+        }
+        return root;
+    }
 }
