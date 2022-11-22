d4e4a9bc3636832dc48c5f1d49ec6a152c91b848
catehuang <s0350228@gmail.com>
2022-11-22 12:04:04 -0700

    clean content

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import common.TreeNode;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.PriorityQueue;
@@ -5,16 +7,9 @@ import java.util.PriorityQueue;
 class Main {
     public static void main(String[] args)
     {
-        List<String> list1 = new ArrayList<>();
-        list1.add( "One" );
-        list1.add( "Two" );
-        list1.add( "Three" );
 
-        List<String> list2 = new ArrayList<>();
-        list2.add("Four");
-
-        list1.remove( list2 );
-        System.out.println(list1);
     }
+
+
 }
 
