7a48ea94bbe5a3865a9b6ceffeb9ada34c00c2e8
catehuang <s0350228@gmail.com>
2022-11-29 17:48:34 -0700

    add another way to solve problem - slower..

--- src/easy/_242_ValidAnagram.java
+++ src/easy/_242_ValidAnagram.java
@@ -1,6 +1,8 @@
 package easy;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 
 /**
  * Given two strings s and t, return true if t is an anagram of s,
@@ -11,6 +13,43 @@ import java.util.Arrays;
  */
 public class _242_ValidAnagram {
     public boolean isAnagram(String s, String t) {
+        return solution_1(s, t);
+        // return solution_2(s, t);
+    }
+
+    private boolean solution_2(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i), 1);
+            } else {
+                map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
+            }
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            if (map.containsKey(t.charAt(i))) {
+                if (map.get(t.charAt(i)) > 1) {
+                    map.put(t.charAt(i), map.get(t.charAt(i)) - 1);
+                } else if (map.get(t.charAt(i)) == 1) {
+                    map.remove(t.charAt(i));
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+
+        return map.isEmpty();
+    }
+
+    private boolean solution_1(String s, String t) {
         char[] a = s.toCharArray();
         char[] b = t.toCharArray();
         Arrays.sort(a);
