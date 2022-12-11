7aeaef8edec39d8a8612442c03a50e24606c73b4
catehuang <s0350228@gmail.com>
2022-12-11 14:01:28 -0700

    205 add anther strategy

--- src/easy/_205_IsomorphicStrings.java
+++ src/easy/_205_IsomorphicStrings.java
@@ -13,7 +13,36 @@ import java.util.List;
  */
 public class _205_IsomorphicStrings {
     public boolean isIsomorphic(String s, String t) {
-        return solution_1(s, t);
+        //return solution_1(s, t);
+
+        // a map stores characters mapping between two strings
+        HashMap<Character, Character> map = new HashMap<>();
+        // a map stores the status which duplicate or not
+        HashMap<Character, Boolean> charactersMapped = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char skey = s.charAt(i);
+            if (map.containsKey(skey)) {
+                // compare the map
+                char mapped = map.get(skey);
+                if (mapped != t.charAt(i)) {
+                    return false;
+                }
+            } else {
+                // create a map between s and t
+                // is the character of t mapped before?
+                    // no -> create one relationship between s and t and indicate t is mapped
+                    // yes -> how come!
+                char tkey = t.charAt(i);
+                if (charactersMapped.containsKey(tkey)) {
+                    return false;
+                } else {
+                    charactersMapped.put(tkey, true);
+                    map.put(skey, tkey);
+                }
+            }
+        }
+        return true;
     }
 
     private boolean solution_1(String s, String t) {
